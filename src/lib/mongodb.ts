d961896ebbdbe6203e068f515b1065e517dea141
Amri2k <135651719+Riamta@users.noreply.github.com>
2025-04-25 19:31:11 +0700

    313

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -5,13 +5,13 @@ export const inMemoryPageViews: Record<string, number> = {};
 
 // Get MongoDB client if available
 export async function getMongoClient() {
-  if (!process.env.MONGODB_URI) {
+  if (!process.env.NEXT_PUBLIC_MONGODB_URI) {
     console.log('MongoDB URI not found');
     return null;
   }
 
   try {
-    const client = new MongoClient(process.env.MONGODB_URI, {
+    const client = new MongoClient(process.env.NEXT_PUBLIC_MONGODB_URI, {
       connectTimeoutMS: 5000,
       serverSelectionTimeoutMS: 5000,
       socketTimeoutMS: 5000,
