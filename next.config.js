68e458a1df7d90dba0c8628d905239fe9353c527
Amri2k <135651719+Riamta@users.noreply.github.com>
2025-04-26 11:04:12 +0700

    113

--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ const nextConfig = {
   images: {
     domains: ['img.youtube.com'],
   },
+  webpack: (config, { isServer }) => {
+    // Handle cheerio and undici
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig 
